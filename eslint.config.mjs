b559993c2cd4f174cb0ac04bd3359ad38844b074
ArturFox <arturfox1317@gmail.com>
2025-08-11 22:48:47 +0500

    HELP

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-this-alias": "off",
     },
   },
 ];
